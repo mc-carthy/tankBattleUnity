0ba067a52c59fd84f8a59f2cb06610950850d8c9
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-14 12:19:24 +0000

    Create PlayerMotor script to control tank and turret

---
+++ Assets/scripts/PlayerMotor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+	[SerializeField]
+	private Transform chassis;
+	[SerializeField]
+	private Transform turret;
+
+	private Rigidbody rb;
+	private float moveSpeed = 100f;
+	private float chassisRotateSpeed = 1f;
+	private float turretRotateSpeed = 3f;
+
+	private void Awake () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	public void RotateChassis (Vector3 dir) {
+		FaceDirection(chassis, dir, chassisRotateSpeed);
+	}
+
+	public void RotateTurret (Vector3 dir) {
+		FaceDirection(turret, dir, turretRotateSpeed);
+	}
+
+	private void MovePlayer (Vector3 dir) {
+		Vector3 moveDirection = dir * moveSpeed * Time.deltaTime;
+		rb.velocity = moveDirection;
+	}
+
+	private void FaceDirection (Transform xform, Vector3 dir, float rotSpeed) {
+		if (dir != Vector3.zero && xform != null) {
+			Quaternion desiredRot = Quaternion.LookRotation(dir);
+
+			xform.rotation = Quaternion.Slerp(xform.rotation, desiredRot, rotSpeed * Time.deltaTime);
+		}
+	}
+}
