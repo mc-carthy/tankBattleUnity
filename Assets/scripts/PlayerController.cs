002475dad899a3a875acc3d2fa63795b5debc034
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-14 16:05:47 +0000

    Create PlayerController

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerHealth))]
+[RequireComponent(typeof(PlayerMotor))]
+[RequireComponent(typeof(PlayerSetup))]
+[RequireComponent(typeof(PlayerShoot))]
+public class PlayerController : MonoBehaviour {
+
+	private PlayerHealth pHealth;
+	private PlayerMotor pMotor;
+	private PlayerSetup pSetup;
+	private PlayerShoot pShoot;
+
+	private void Awake () {
+		pHealth = GetComponent<PlayerHealth>();
+		pMotor = GetComponent<PlayerMotor>();
+		pSetup = GetComponent<PlayerSetup>();
+		pShoot = GetComponent<PlayerShoot>();
+	}
+
+	private Vector2 GetInput () {
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+
+		return new Vector3 (h, 0, v);
+	}
+
+}
