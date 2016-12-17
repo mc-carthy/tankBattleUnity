bc704deb077f8dde1a9dbf3875ad43e6d8770937
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-17 22:40:05 +0000

    Create billboard player label canvas for tank prefab

---
+++ Assets/scripts/FaceCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FaceCamera : MonoBehaviour {
+
+	private Camera cam;
+
+	private void Start () {
+		if (cam == null) {
+			cam = Camera.main;
+		}
+	}
+
+	private void Update () {
+		transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward, cam.transform.rotation * Vector3.up);
+	}
+}
--- Assets/scripts/PlayerSetup.cs
+++ Assets/scripts/PlayerSetup.cs
@@ -3,5 +3,16 @@ using UnityEngine.Networking;
 
 public class PlayerSetup : NetworkBehaviour {
 
+	[SerializeField]
+	private Color playerColor;
+
+	public override void OnStartLocalPlayer () {
+		MeshRenderer[] meshes = GetComponentsInChildren<MeshRenderer>();
+
+		foreach (MeshRenderer mesh in meshes) {
+			mesh.material.color = playerColor;
+		}
+	}
+
 
 }
