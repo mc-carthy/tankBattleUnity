7f88622d7b7a521b15f9afabefb88974c5d7b08c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-17 22:44:19 +0000

    Only the local player is labelled

--- Assets/scripts/PlayerSetup.cs
+++ Assets/scripts/PlayerSetup.cs
@@ -1,16 +1,38 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class PlayerSetup : NetworkBehaviour {
 
 	[SerializeField]
 	private Color playerColor;
+	[SerializeField]
+	private string baseName = "Player";
+	[SerializeField]
+	private int playerNum = 1;
+	[SerializeField]
+	private Text playerNameText;
+
+	public override void OnStartClient () {
+		base.OnStartClient();
+
+		if (playerNameText != null) {
+			playerNameText.enabled = false;
+		}
+	}
 
 	public override void OnStartLocalPlayer () {
+		base.OnStartLocalPlayer();
+
 		MeshRenderer[] meshes = GetComponentsInChildren<MeshRenderer>();
 
 		foreach (MeshRenderer mesh in meshes) {
 			mesh.material.color = playerColor;
+		}
+
+		if (playerNameText != null) {
+			playerNameText.enabled = true;
+			playerNameText.text = baseName + playerNum.ToString();
 		}
 	}
 
