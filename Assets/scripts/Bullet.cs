7decfa84c6b1116c885807a1a478e0a1468bb21d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-18 15:53:33 +0000

    Bullet faces velocity vector

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -3,6 +3,8 @@ using UnityEngine.Networking;
 using System.Collections.Generic;
 using System.Linq;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
 public class Bullet : NetworkBehaviour {
 
 	[SerializeField]
@@ -16,9 +18,19 @@ public class Bullet : NetworkBehaviour {
 	[SerializeField]
 	private float lifetime = 5f;
 
+	private Rigidbody rb;
+	private Collider col;
 	private List<ParticleSystem> allParticles;
 
-	private void Start () {
+	private void Awake () {
+		rb = GetComponent<Rigidbody>();
+		col = GetComponent<Collider>();
 		allParticles = GetComponentsInChildren<ParticleSystem>().ToList();
+	}
+
+	private void OnCollisionExit (Collision other) {
+		if (rb.velocity != Vector3.zero) {
+			transform.rotation = Quaternion.LookRotation(rb.velocity);
+		}
 	}
 }
